d5437959a23989f376ccb1bd7d69b83ed79e9372
lauragchiarello <laura_chiarello@live.com>
2024-08-19 13:46:22 -0300

    tentativa 2

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function generateCards() {
     cardElement.setAttribute("data-name", card.name);
     cardElement.innerHTML = `
       <div class="front">
-        <img class="front-image" src=${card.image} />
+        <img class="front-image" src=${card.image}/>
       </div>
       <div class="back"></div>
     `;
